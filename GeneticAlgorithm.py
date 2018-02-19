afa10ff0dcc2bdcb89e12c01985a7554d3d8a667
Anjalie Kini <askmac@anjalies-mbp.andover.edu>
2018-02-19 08:38:24 -0500

    Fixed all compile errors and got the main test case working

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -2,6 +2,7 @@ from random import random
 from abc import abstractmethod
 import copy
 import os, datetime
+import math
 
 
 class GeneticAlgorithm:
@@ -10,195 +11,207 @@ class GeneticAlgorithm:
             raise Exception("Invalid crossoverRate: " + str(crossoverRate))
         if (mutationRate < 0 or mutationRate > 1):
             raise Exception("Invalid mutationRate: " + str(mutationRate))
-        
+
         self.crossoverPolicy = crossoverPolicy
         self.crossoverRate = crossoverRate
         self.mutationPolicy = mutationPolicy
         self.mutationRate = mutationRate
         self.selectionPolicy = selectionPolicy
-    
+
     def getCrossoverPolicy(self):
         return self.crossoverPolicy
-    
+
     def getCrossoverRate(self):
         return self.crossoverRate
-    
-    def getCrossoverPolicy(self):
-        return self.crossoverPolicy
-    
-    def getCrossoverRate(self):
-        return self.crossoverRate
-    
+
+    def getMutationPolicy(self):
+        return self.mutationPolicy
+
+    def getMutationRate(self):
+        return self.mutationRate
+
+    def getSelectionPolicy(self):
+        return self.selectionPolicy
+
     def evolve(self, initialPopulation, stoppingCondition):
         currentPopulation = initialPopulation
         generationCount = 0
         while (not stoppingCondition.isSatisfied(currentPopulation)):
-            currentPopulation = nextGeneration(currentPopulation)
+            currentPopulation = self.nextGeneration(currentPopulation)
             generationCount = generationCount + 1
         return currentPopulation
-    
-    
+
+
     def nextGeneration(self, currentPopulation):
         nextGeneration = currentPopulation.nextGeneration()
         while (nextGeneration.getPopulationSize() < nextGeneration.getPopulationLimit()):
             # select parent chromosomes
             pair = self.getSelectionPolicy().select(currentPopulation)
-            
+
             # crossover?
-            if (random() < getCrossoverRate()):
+            if (random() < self.getCrossoverRate()):
                 pair = self.getCrossoverPolicy().crossover(pair.getFirst(), pair.getSecond())
-            
+
             # mutation?
-            if (random() < getMutationRate()):
+            if (random() < self.getMutationRate()):
                 # apply mutation policy
-                pair = self.getMutationPolicy().mutate(pair.getFirst())
-                pair = self.getMutationPolicy().mutate(pair.getSecond())
-            
+                pair = ChromosomePair(self.getMutationPolicy().mutate(pair.getFirst()), self.getMutationPolicy().mutate(pair.getSecond()))
+
             # add the first chromosome to the population
             nextGeneration.addChromosome(pair.getFirst())
-            # still placefor the second chromosome?
+            # still room for the second chromosome?
             if (nextGeneration.getPopulationSize() < nextGeneration.getPopulationLimit()):
                 # add the second chromosome to the population
                 nextGeneration.addChromosome(pair.getSecond())
-        
-    return nextGeneration
+        return nextGeneration
 
 
 
 class Population:
-    def __init__(self, populationLimit, chromosomes):
+    def __init__(self, populationLimit, thresholdRate, chromosomes=None):
         if (populationLimit < 0):
             raise Exception("Invalid populationLimit: " + str(populationLimit))
-        if (populationLimit < 0):
-            raise Exception("Invalid populationLimit: " + str(populationLimit))
-        if (len(chromosomes) > populationLimit):
+        if (thresholdRate >= 1):
+            raise Exception("Invalid thresholdRate: " + str(thresholdRate))
+        if (chromosomes is not None and len(chromosomes) > populationLimit):
             raise Exception("Size of population (" + len(chromosomes) + ") exceeds populationLimit: " + str(populationLimit))
-        
         self.populationLimit = populationLimit
-        self.chromosomes = copy.deepcopy(chromosomes)
-    
+        self.thresholdRate = thresholdRate
+        if (chromosomes is not None):
+            self.chromosomes = copy.deepcopy(chromosomes)
+        else:
+            self.chromosomes = []
+
     def getPopulationLimit(self):
         return self.populationLimit
-    
+
+    def getThresholdRate(self):
+        return self.thresholdRate
+
     def getChromosomes(self):
         return self.chromosomes
-    
+
     def addChromosome(self, newChromosome):
-        if (len(chromosomes) >= populationLimit):
-            raise Exception("Size of population (" + len(chromosomes) + ") exceeds populationLimit: " + str(populationLimit))
-        self.chromosomes.add(newChromosome)
-    
+        if (len(self.chromosomes) >= self.populationLimit):
+            raise Exception("Size of population (" + len(self.chromosomes) + ") exceeds populationLimit: " + str(self.populationLimit))
+        self.chromosomes.append(newChromosome)
+
     def getFittestChromosome(self):
         bestSoFar = self.chromosomes[0]
         for nextChromosome in self.chromosomes:
             if (nextChromosome.compareTo(bestSoFar) > 0):
                 bestSoFar = nextChromosome
         return bestSoFar
-    
+
     def setPopulationLimit(self, limit):
         if (limit < 0):
             raise Exception("Invalid populationLimit input: " + str(limit))
         if (len(chromosomes) > limit):
             raise Exception("Size of population (" + len(chromosomes) + ") exceeds input limit: " + str(limit))
         self.populationLimit = limit
-    
+
+    def nextGeneration(self):
+        # initialize a new generation with the same parameters
+        nextGeneration = Population(self.getPopulationLimit(), self.getThresholdRate());
+        oldChromosomes = self.getChromosomes();
+        oldChromosomes.sort(key = lambda x: x.fitness, reverse=True);
+
+        # find last "not good enough" chromosome
+        boundIndex = int(math.ceil((1.0 - self.getThresholdRate()) * len(oldChromosomes)))
+        for i in range(boundIndex, len(oldChromosomes)):
+            nextGeneration.addChromosome(oldChromosomes[i])
+        return nextGeneration;
+
     def getPopulationSize(self):
-        return len(chromosomes)
+        return len(self.chromosomes)
 
 
 
 # Immutable, so their fitness is also immutable and can be cached
 class Chromosome:
     NO_FITNESS = -1000000
-        
-        def __init__(self, rep):
+
+    def __init__(self, rep):
         self.representation = copy.copy(rep)
-    
-    # Changes a randomly chosen element of the representation
-    #  to a random value distributed in [0,1]
+        self.fitness = Chromosome.NO_FITNESS
+
+    @abstractmethod
     def getFitness(self):
-        if (self.fitness == NO_FITNESS)
-            self.fitness = fitness()
-        return self.fitness
-        
-        def compareTo(self, another):
-            if self.fitness > another.getFitness():
-                return 1
-                elif self.fitness < another.getFitness():
-                    return -1
-                else
-                    return 0
-            
-            # should be true if identical representation,
-            def isSame(self, another):
-                        return False
-                    
-                    def findSameChromosome(self, population):
-                        for anotherChr in population:
-                    if (self.isSame(anotherChr):
-                        return anotherChr
-                        
-                        return None
-                        
-                        def searchForFitnessUpdate(self, population):
-                        sameChr = self.findSameChromosome(population)
-                        if (sameChr != None):
-                        self.fitness = sameChr.getFitness()
-                        
-                        def getRepresentation(self):
-                        return self.representation
-                        
-                        def getLength(self):
-                        return getRepresentation().size()
-                        
-                        def __str__(self):
-                        return ("F: " + str(self.getFitness()) +
-                                ", R: " + str(self.getRepresentation()))
-                        
-                        def __repr__(self):
-                        return ("Fitness: " + str(self.getFitness()) +
-                                ", Representation: " + str(self.getRepresentation()))
-                        
-                        
-                        class ChromosomePair:
-                        def __init__(self, chr1, chr2):
-                        self.first = chr1
-                        self.second = chr2
-                        
-                        def getFirst(self):
-                        return self.first
-                        
-                        def getSecond(self):
-                        return self.second
-                        
-                        def __str__(self):
-                        return ("C1: " + str(first) +
-                                ", C2: " + str(second))
-                        
-                        def __repr__(self):
-                        return ("Chromosome1: " + str(first) +
-                                ", Chromosome2: " + str(second))
-                        
-                        
-                        class CrossoverPolicy:
-                        @abstractmethod
-                        def crossover(self, firstChromosome, secondChromosome):
-                        pass
-                        
-                        class MutationPolicy:
-                        # Changes a randomly chosen element of the representation 
-                        @abstractmethod
-                        def mutate(self, originalChromosome):
-                        pass
-                        
-                        class SelectionPolicy:
-                        @abstractmethod
-                        def select(self, population):
-                        pass
-                        
-                        class StoppingCondition:
-                        @abstractmethod
-                        def isSatisfied(self, population):
-                        pass
-                        
-                        
+        pass
+
+    def compareTo(self, another):
+        if self.fitness > another.getFitness():
+            return 1
+        elif self.fitness < another.getFitness():
+            return -1
+        else:
+            return 0
+
+    @abstractmethod
+    def isSame(self, another):
+        pass
+
+    def findSameChromosome(self, population):
+        for anotherChr in population:
+            if (self.isSame(anotherChr)):
+                return anotherChr
+        return None
+
+    def searchForFitnessUpdate(self, population):
+        sameChr = self.findSameChromosome(population)
+        if (sameChr != None):
+            self.fitness = sameChr.getFitness()
+
+    def getRepresentation(self):
+        return self.representation
+
+    def getLength(self):
+        return len(self.getRepresentation())
+
+    def __str__(self):
+        return ("F: " + str(self.getFitness()) + ", R: " + str(self.getRepresentation()))
+
+    def __repr__(self):
+        return ("Fitness: " + str(self.getFitness()) + ", Representation: " + str(self.getRepresentation()))
+
+
+class ChromosomePair:
+    def __init__(self, chr1, chr2):
+        self.first = chr1
+        self.second = chr2
+
+    def getFirst(self):
+        return self.first
+
+    def getSecond(self):
+        return self.second
+
+    def __str__(self):
+        return ("C1: " + str(first) + ", C2: " + str(second))
+
+    def __repr__(self):
+        return ("Chromosome1: " + str(first) + ", Chromosome2: " + str(second))
+
+
+class CrossoverPolicy:
+    @abstractmethod
+    def crossover(self, firstChromosome, secondChromosome):
+        pass
+
+class MutationPolicy:
+    # Changes a randomly chosen element of the representation 
+    @abstractmethod
+    def mutate(self, originalChromosome):
+        pass
+
+class SelectionPolicy:
+    @abstractmethod
+    def select(self, population):
+        pass
+
+class StoppingCondition:
+    @abstractmethod
+    def isSatisfied(self, population):
+        pass
+
+
--- geneticTest.py
+++ geneticTest.py
@@ -1,137 +1,149 @@
 import os, datetime
-import geneticAlgorithm
+import GeneticAlgorithm
 from random import randint
 import copy
 
-class StringChromosome(Chromosome):
-	def __init__(self, rep, target):
-		this.target = target
-        super(rep)
+class StringChromosome(GeneticAlgorithm.Chromosome):
+    def __init__(self, rep, target):
+        self.target = target
+        super().__init__(rep)
 
-	def fitness(self):
-	    f=0 # best fitness
-		myRep = getRepresentation()
-	    for i in range(len(target)):
-			f = f - abs(ord(target[i]) - ord(myRep[i]))
-		return f
-		
-	def checkValidity(self, rep):
-	    for c in rep:
-		    if (ord(c) < 32 or ord(c) > 126):
-			    raise Exception("Invalid char in representation: " + str(c))
-	
-	def newFixedLengthChromosome(rep)
-	    return StringChromosome(rep, target)
+    def getFitness(self):
+        # compute once
+        if (self.fitness == GeneticAlgorithm.Chromosome.NO_FITNESS):
+            f=0 # best fitness
+            myRep = self.getRepresentation()
+            listTgt = list(self.target)
+            for i in range(len(listTgt)):
+                # print('listTgt[i]=' + str(listTgt[i]) + ", myRep[i]=" + str(myRep[i]))
+                f = f - abs(ord(listTgt[i]) - ord(myRep[i]))
+            self.fitness = f
+        return self.fitness
 
-		    
-class RandomCharMutation(MutationPolicy):
+    def checkValidity(self, inRep):
+        for c in inRep:
+            if (ord(c) < 32 or ord(c) > 126):
+                raise Exception("Invalid char in representation: " + str(c))
+
+    def isSame(self, another):
+        rep1 = self.getRepresentation()
+        rep2 = another.getRepresentation()
+        if (cmp(rep1, rep2) == 0):
+            return True
+        else:
+            return False
+
+    def newFixedLengthChromosome(self, rep):
+        return StringChromosome(rep, self.target)
+
+
+class RandomCharMutation(GeneticAlgorithm.MutationPolicy):
     def __init__(self):
         pass
-		
-	def mutate(original):
+
+    def mutate(self, original):
         characters = original.getRepresentation()
-		mutationIndex = randint(len(characters))
-		mutatedRep = copy.copy(characters)
-		# Desired ASCII range has start=27, end=132
-		newValue = 32 + randint(127-32)
-		mutatedRep.set(mutationIndex, newValue)
-		return original.newFixedLengthChromosome(mutatedRep)
-		
-class OnePointCrossover(CrossoverPolicy):
+        mutationIndex = randint(0, len(characters)-1)
+        mutatedRep = copy.copy(characters)
+        # Desired ASCII range
+        newValue = randint(32, 126)
+        mutatedRep[mutationIndex] = chr(newValue)
+        return original.newFixedLengthChromosome(mutatedRep)
+
+class OnePointCrossover(GeneticAlgorithm.CrossoverPolicy):
     def __init__(self):
         pass
 
-	def crossover(self, firstChromo, secondChromo):
-        firstLength = firstChromo.getLength();
-        if (firstLength != secondChromo.getLength()) {
+    def crossover(self, firstChromo, secondChromo):
+        firstLength = firstChromo.getLength()
+        if (firstLength != secondChromo.getLength()):
             raise Exception("Length of first chromosome (" + str(firstLength) + ") does not match length of the second: " + str(secondChromo.getLength()))
-        }
 
-        // representations of the parents
+        # representations of the parents
         parent1Rep = firstChromo.getRepresentation();
         parent2Rep = secondChromo.getRepresentation();
 
-        // and the children
+        # and the children
         child1Rep = [];
         child2Rep = [];
 
-        // select a crossover point at random 
-        crossoverIndex = 1 + (randInt(0, length-2));
+        # select a crossover point at random 
+        crossoverIndex = 1 + (randint(0, firstLength-2))
 
-        // copy the first part
-        for i in range(crossoverIndex) {
-            child1Rep.add(parent1Rep[i]);
-            child2Rep.add(parent2Rep[i]);
-        }
-        // and switch the second part
-        for (i in range(crossoverIndex, firstLength) {
-            child1Rep.add(parent2Rep[i]);
-            child2Rep.add(parent1Rep[i]);
-        }
+        # copy the first part
+        for i in range(crossoverIndex):
+            child1Rep.append(parent1Rep[i])
+            child2Rep.append(parent2Rep[i])
 
-        return ChromosomePair(firstChromo.newFixedLengthChromosome(child1Rep),
-                              secondChromo.newFixedLengthChromosome(child2Rep));
-							  
-class TournamentSelection(SelectionPolicy):
+        # and switch the second part
+        for i in range(crossoverIndex, firstLength):
+            child1Rep.append(parent2Rep[i])
+            child2Rep.append(parent1Rep[i])
+
+        return GeneticAlgorithm.ChromosomePair(firstChromo.newFixedLengthChromosome(child1Rep), secondChromo.newFixedLengthChromosome(child2Rep));
+
+class TournamentSelection(GeneticAlgorithm.SelectionPolicy):
     def __init__(self, arity):
         self.numberToDraw = arity
 
     def select(self, population):
-        return ChromosomePair(tournament(population), tournament(population))
+        return GeneticAlgorithm.ChromosomePair(self.tournament(population), self.tournament(population))
 
     def tournament(self, inPopulation):
-        if (len(inPopulation) < this.numberToDraw):
-            raise Exception("Size of population (" + len(inPopulation) + ") must be at least: " + str(numberToDraw))
-        tournamentPopulation = Population(this.numberToDraw)
-        myChromosomes = copy.deepcopy(inPopulation)
-        for i in range(this.numberToDraw):
-            rando = randint(0, len(myChromosomes))
-            tournamentPopulation.add(myChromosomes[rando])
+        if (inPopulation.getPopulationSize() < self.numberToDraw):
+            raise Exception("Size of population (" + str(inPopulation.getPopulationSize()) + ") must be at least: " + str(numberToDraw))
+        tournamentPopulation = GeneticAlgorithm.Population(self.numberToDraw, 0.2)
+        myChromosomes = copy.deepcopy(inPopulation.getChromosomes())
+        for i in range(self.numberToDraw):
+            rando = randint(0, len(myChromosomes)-1)
+            tournamentPopulation.addChromosome(myChromosomes[rando])
             myChromosomes.pop(rando)
-        return tournamentPopulation.getFittestChromosome()
+        retVal = tournamentPopulation.getFittestChromosome()
+        # print("tournament winner: " + str(retVal))
+        return retVal
 
     def getArity(self):
-        return selfnumberToDraw
+        return self.numberToDraw
 
     def setArity(self, arity):
         self.numberToDraw = arity
-		
-class FitnessCondition(StoppingCondition):
+
+class FitnessCondition(GeneticAlgorithm.StoppingCondition):
     def __init__(self, threshold):
         self.generationNum = 0
-		self.fitnessThreshold = threshold
+        self.fitnessThreshold = threshold
 
-	def isSatisfied(self, population):
-	    fittestChr = population.getFittestChromosome()
-		if (generationNum == 1 or generationNum % 10 == 0):
-		    print('Generation #' + str(generationNum) + ": " + str(fittestChr))
-		generationNum = generationNum + 1
+    def isSatisfied(self, population):
+        fittestChr = population.getFittestChromosome()
+        if (self.generationNum % 3 == 0):
+            print('%%% Generation #' + str(self.generationNum) + ": (" + str(datetime.datetime.now()) + ") fittest=" + str(fittestChr))
+        self.generationNum = self.generationNum + 1
 
-		fittestFitness = fittestChr.getFitness()
-		if (fittestFitness < fitnessThreshold):
-		    return True
-		else
-			return False
+        fittestFitness = fittestChr.getFitness()
+        if (fittestFitness > self.fitnessThreshold):
+            return True
+        else:
+            return False
 
 def getInitialPopulation():
     TARGET_STRING = "Phillips Andover"
     popList = []
-	for i in range(100):
-	    popList.add(StringChromosome(randomRepresentation(len(TARGET_STRING)), TARGET_STRING)
-	return popList
+    for i in range(100):
+        popList.append(StringChromosome(randomRepresentation(len(TARGET_STRING)), list(TARGET_STRING)))
+    return GeneticAlgorithm.Population(len(popList), 0.2, popList)
 
 def randomRepresentation(size):
-	newRep = []
-    for i in size:
-		newRep.add(32 + randint(127-32))
-	return newRep
+    newRep = []
+    for i in range(size):
+        newRep.append(chr(randint(32, 126)))
+    return newRep
 
 ####################################################################################################################
 def main():
     print ("####### Current time = " + str(datetime.datetime.now())) 
-	algo = GeneticAlgorithm(OnePointCrossover(), 0.9, RandomCharMutation(), 0.03, TournamentSelection(2))
-	initialPop = getInitialPopulation()
-	finalPopulation = algo.evolve(initial, FitnessCondition(0.0001))
+    algo = GeneticAlgorithm.GeneticAlgorithm(OnePointCrossover(), 0.9, RandomCharMutation(), 0.03, TournamentSelection(2))
+    initialPop = getInitialPopulation()
+    finalPopulation = algo.evolve(initialPop, FitnessCondition(-3))
     best = finalPopulation.getFittestChromosome()
 
 if __name__ == '__main__':
