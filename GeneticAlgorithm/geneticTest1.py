b399ef2024ed00fc1c0b52e0d35b4fb0b9ab6292
Anjalie Kini <askmac@Anjalies-MBP.andover.edu>
2018-02-23 08:45:37 -0500

    Added two geneticAlgorithm tests

---
+++ GeneticAlgorithm/geneticTest1.py
@@ -0,0 +1,29 @@
+import os, datetime
+import GeneticAlgorithm
+from random import randint
+
+def getInitialPopulation():
+    TARGET_STRING = "Phillips Andover"
+    popList = []
+    for i in range(100):
+        popList.append(GeneticAlgorithm.StringChromosome(randomRepresentation(len(TARGET_STRING)), list(TARGET_STRING)))
+    return GeneticAlgorithm.Population(len(popList), 0.2, popList)
+
+# Returns list of ASCII chars
+def randomRepresentation(size):
+    newRep = []
+    for i in range(size):
+        newRep.append(chr(randint(32, 126)))
+    return newRep
+
+####################################################################################################################
+def main():
+    print ("####### Current time at start = " + str(datetime.datetime.now())) 
+    algo = GeneticAlgorithm.GeneticAlgorithm(GeneticAlgorithm.OnePointCrossover(), 0.9, GeneticAlgorithm.RandomCharMutation(), 0.03, GeneticAlgorithm.TournamentSelection(2))
+    initialPop = getInitialPopulation()
+    finalPopulation = algo.evolve(initialPop, GeneticAlgorithm.FitnessCondition(-30))
+    best = finalPopulation.getFittestChromosome()
+    print ("####### Current time at end = " + str(datetime.datetime.now())) 
+
+if __name__ == '__main__':
+    main()
